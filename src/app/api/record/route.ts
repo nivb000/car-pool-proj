f3c93cfdf13eb260ee9ae79d649cd05a9faa2ce4
nivb000 <bniv000@gmail.com>
2024-06-02 15:01:31 +0300

    added server route, data table

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -1,11 +1,18 @@
-export async function GET() {
-    const res = await fetch('https://data.mongodb-api.com/...', {
-        headers: {
-            'Content-Type': 'application/json',
-            'API-Key': process.env.DATA_API_KEY,
-        },
-    })
-    const data = await res.json()
+const demoData = require("@/services/demoData")
 
-    return Response.json({ data })
+
+export async function GET() {
+    let entities = demoData
+    return Response.json(entities)
 }
+// export async function GET() {
+//     const res = await fetch('https://data.mongodb-api.com/...', {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'API-Key': process.env.DATA_API_KEY,
+//         },
+//     })
+//     const data = await res.json()
+
+//     return Response.json({ data })
+// }
--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,20 +1,17 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { recordService } from '../../services/record.service.local'
+import { Record } from '@/interfaces/record'
+import { RecordTable } from '@/cmps/record-table'
 
+// TODO:MUI TABLE
 
 async function getRecords() {
-    const res = await recordService.query()
-    return res
-    
+  const res = await fetch('http://localhost:3000/api/record')
+  return res.json()
 }
-
 
 const RecordApp = async () => {
 
+  const { entities: records }: { entities: Record[] } = await getRecords() || []
 
-  const records = await getRecords()
 
   return <section className='record-app'>
     <div className='gradient-overlay'>
@@ -22,8 +19,12 @@ const RecordApp = async () => {
       </div>
     </div>
     <section className='main-layout flex main-container'>
-      <div className='left'></div>
-      <div className='right'></div>
+      <div className='left'>
+      </div>
+      <div className='right'>
+        <RecordTable />
+        <div>{records.map(record => <div key={record._id}>{record.startingKm}</div>)}</div>
+      </div>
     </section>
   </section>
 }
