cc39389366898f6afdf6c09a25c6c717aa2b136d
nivb000 <bniv000@gmail.com>
2024-06-27 11:16:57 +0300

    push try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation"
 export default function Home() {
 
   // TODO: CHECK IF LOGGED IN REDIRECT RECORD ELSE REDIRECT LOGIN
-  redirect('/record')
+  // redirect('/record')
 
   return <section className="home-page">
     <h3>Hi from homepage</h3>
